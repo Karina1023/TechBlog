1aea5eea098e3944349dbcc85af611d3df036dca
Karina Ayala <karinaayala2021@u.northwestern.edu>
2021-06-09 00:08:48 -0500

    updating files

--- server.js
+++ server.js
@@ -7,12 +7,11 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
-const { Sequelize } = require('sequelize/types');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const session = {
+const sess = {
     secret: 'Super secret',
     cookie: {},
     resave: false,
